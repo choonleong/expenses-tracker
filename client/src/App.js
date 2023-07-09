a8171c6b685311e2b2245a10c24288cc886fb727
Choon Leong <musiziano@gmail.com>
2023-07-09 23:15:08 +0800

    insert router

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Wow this is the first router</div>,
+  },
+]);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>This has been edited.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
